7e71d728a77ecfede9b6fe8b1c074dd5f66999bf
Suchiz <kyuuzuka@hotmail.fr>
2017-05-07 22:36:30 +0100

    add player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector2 mouseLook;
+	private Vector2 smooth;
+	public float sensitivity = 5.0f;
+	public float smoothing = 2.0f;	
+
+	private GameObject character;
+
+	void Start () {
+		character = this.transform.parent.gameObject;
+	}
+	
+
+	void Update () {
+		var md = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+		md = Vector2.Scale (md, new Vector2 (sensitivity * smoothing, sensitivity * smoothing));
+
+		smooth.x = Mathf.Lerp (smooth.x, md.x, 1f / smoothing);
+		smooth.y = Mathf.Lerp (smooth.y, md.y, 1f / smoothing);
+
+		mouseLook += smooth;
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -90.0f, 90.0f);
+
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
+	}
+}
---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public float jumpForce = 6.5f;
+	public Rigidbody rb;
+
+	private Vector3 jump;
+	private bool jumping = false;
+
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody> ();
+		jump = new Vector3 (0, jumpForce, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Move right + left
+		float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		transform.Translate (moveX, 0, moveZ);
+
+
+		//Jump
+		if (Input.GetKeyDown (KeyCode.Space) && !jumping) {
+			rb.velocity += jump;
+			jumping = true;
+		}
+		jumping = false;
+
+		//Quit first person
+		if (Input.GetKeyDown("escape"))
+			Cursor.lockState = CursorLockMode.None;
+		
+	}
+}
