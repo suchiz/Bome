0a5279be979d25373a809a42758dd3fb3d68792a
styloInt <penspinningfr@gmail.com>
2017-05-08 23:32:38 -0400

    begenning of chrono

---
+++ Assets/Scripts/HandleChrono.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleChrono : MonoBehaviour {
+	Text chrono;
+	private float chronoCount;
+
+	// Use this for initialization
+	void Start () {
+		chrono = GetComponent<Text> ();
+		chronoCount = 0;
+		displayChrono ();
+		Debug.Log ("Here : " + chrono.text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		chronoCount += Time.deltaTime;
+		displayChrono ();
+		Debug.Log ("Update : " + chrono.text);
+	}
+
+	void displayChrono()
+	{
+		chrono.text = chronoCount.ToString();
+	}
+}
