01fbcce9f9e37b05fb6c5b91ce43b1deab3c236b
styloInt <penspinningfr@gmail.com>
2017-05-09 22:36:08 -0400

    score handled

--- Assets/Scripts/CharacterCollider.cs
+++ Assets/Scripts/CharacterCollider.cs
@@ -1,17 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class CharacterCollider : MonoBehaviour {
 
 	public GameObject winText;
+	public GameObject chronoText;
+	public InputField pseudoInput;
+	public Canvas scoreCanvas;
+	public Text listScoreText;
 
+	private string fileNameScore;
+	private FirstPersonController fp;
+	private CameraController cc;
+
+	private bool allowEnter = false;
 	void Start (){
 		winText.SetActive (false);
+		fileNameScore = "score.txt";
+		pseudoInput.text = "Enter your pseudo ...";
+		fp = GetComponent<FirstPersonController> ();
+		scoreCanvas.gameObject.SetActive(false);
+		cc = (CameraController)GameObject.Find ("Main Camera").GetComponent ("CameraController");
+	}
+
+	void Update()
+	{
+		if (allowEnter && Input.GetKeyDown (KeyCode.Return)) {
+			pseudoInput.gameObject.SetActive (false);
+			allowEnter = false;
+
+			//We save the score in a file     
+			using(StreamWriter sw = File.AppendText(fileNameScore))
+			{
+				string timeStr = chronoText.GetComponent<HandleChrono> ().getStrTime ();
+				sw.WriteLine(pseudoInput.text + " : " + timeStr);          
+			}
+
+			winText.SetActive (false);
+
+			//Display all the scores
+			scoreCanvas.gameObject.SetActive(true);
+			displayScore ();
+
+		}
+		else {
+			allowEnter = pseudoInput.isFocused;
+		}
+
 	}
 
 	void OnTriggerEnter (Collider other){
-		if(other.gameObject.tag == "Flag")
+		if (other.gameObject.tag == "Flag") {
 			winText.SetActive (true);
+
+			//Display the cursor and desactivate the player Controller
+			Cursor.lockState = CursorLockMode.None;
+			cc.enabled = false;
+			fp.enabled = false;
+
+			// We stop the chrono
+			chronoText.GetComponent<HandleChrono> ().enabled = false;
+		}
+	}
+
+	void displayScore()
+	{
+		listScoreText.text = "";
+
+		// Read the file and display it line by line.
+		string line;
+		StreamReader file = new StreamReader(fileNameScore);
+		while((line = file.ReadLine()) != null)
+			listScoreText.text += line + "\n";
+
+		file.Close();
 	}
 }
--- Assets/Scripts/HandleChrono.cs
+++ Assets/Scripts/HandleChrono.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HandleChrono : MonoBehaviour {
 	Text chrono;
+	private bool win = false;
 	private float chronoCount;
 
 	// Use this for initialization
@@ -16,12 +18,37 @@ public class HandleChrono : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		chronoCount = Time.timeSinceLevelLoad;
-		displayChrono ();
+	}
+
+	void FixedUpdate()
+	{
+		if (!win) {
+			chronoCount = Time.timeSinceLevelLoad;
+			displayChrono ();
+			return;
+		}
+
+		// if win
+
 	}
 
 	void displayChrono()
 	{
-		chrono.text = "Time: " + chronoCount.ToString("F2");
+
+		chrono.text = "Time: " + getStrTime();
 	}
+
+	public string getStrTime()
+	{
+		TimeSpan t = TimeSpan.FromSeconds(chronoCount);
+		string timeStr = string.Format("{1:D2}:{2:D2}:{3:D3}",
+			t.Hours, 
+			t.Minutes, 
+			t.Seconds, 
+			t.Milliseconds);
+
+		return timeStr;
+		
+	}
+		
 }
