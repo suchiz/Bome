557ba174d7ef2659053d1c47d84d1340a63c9429
Suchiz <kyuuzuka@hotmail.fr>
2017-05-10 06:47:20 +0100

    Settings update

---
+++ Assets/Scripts/ScreenOptions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenOptions : MonoBehaviour {
+	
+	private List<string> resolutions = new List<string>() {"800x600", "1024x768"};
+	private bool windowed;
+	private int resolution_chosen = 0;
+	public Dropdown dropdown;
+
+	void Start () {
+		dropdown.AddOptions (resolutions);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (windowed) {
+			if (resolution_chosen == 0)
+				Screen.SetResolution (800, 600, false);
+			else if (resolution_chosen == 1)
+				Screen.SetResolution (1024, 768, false);
+		} else {
+			if (resolution_chosen == 0)
+				Screen.SetResolution (800, 600, true);
+			else if (resolution_chosen == 1)
+				Screen.SetResolution (1024, 768, true);
+		}
+	}
+
+	public void setIndex (int index){
+		resolution_chosen = index;
+	}
+
+	public void setWindowed (bool isOn){
+		windowed = isOn;
+	}
+}
